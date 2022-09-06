af4477fa9d8c4347572d76fd04983c5f1524d21d
bogasravan <sravanboga@gmail.com>
2022-09-06 16:43:07 +0530

    test

--- src/main/java/myntra/Main.java
+++ src/main/java/myntra/Main.java
@@ -2,6 +2,6 @@ package myntra;
 
 public class Main {
 	public static void main(String[] args) {
-		System.out.println("Hello, world!");
+		System.out.println("Hello, This is Maven Project");
 	}
 }
